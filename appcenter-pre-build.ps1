3eb98da027b1985456f5b5a7daa68c126c4ed4ca
Giuseppe Sorrentino <uhpnou@gmail.com>
2021-01-02 21:20:01 +0100

    build fixing 16

--- appcenter-pre-build.ps1
+++
@@ -1,5 +0,0 @@
-# C:/"Program Files (x86)"/"Microsoft Visual Studio"/2019/Enterprise/MSBuild/Current/Bin/msbuild.exe "$($env:APPCENTER_SOURCE_DIRECTORY)\AmethystWindows\AmethystWindows.csproj" /p:Configuration="$($env:APPCENTER_UWP_CONFIGURATION)" /p:Platform=ARM
-
-# C:/"Program Files (x86)"/"Microsoft Visual Studio"/2019/Enterprise/MSBuild/Current/Bin/msbuild.exe "$($env:APPCENTER_SOURCE_DIRECTORY)\AmethystWindows\AmethystWindows.csproj" /p:Configuration="$($env:APPCENTER_UWP_CONFIGURATION)" /p:Platform=x86
-
-# C:/"Program Files (x86)"/"Microsoft Visual Studio"/2019/Enterprise/MSBuild/Current/Bin/msbuild.exe "$($env:APPCENTER_SOURCE_DIRECTORY)\AmethystWindows\AmethystWindows.csproj" /p:Configuration="$($env:APPCENTER_UWP_CONFIGURATION)" /p:Platform=x64
