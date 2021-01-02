3b08f9bbfd8cf97e520d5146e7ca602e072b3a7d
Giuseppe Sorrentino <uhpnou@gmail.com>
2021-01-02 14:55:23 +0100

    build fixing 11

--- appcenter-pre-build.ps1
+++ appcenter-pre-build.ps1
@@ -1,5 +1,5 @@
-C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/MSBuild/Current/Bin/msbuild.exe $APPCENTER_SOURCE_DIRECTORY\AmethystWindows\AmethystWindows.csproj /p:Configuration=$APPCENTER_UWP_CONFIGURATION /p:Platform=ARM
+C:/"Program Files (x86)"/"Microsoft Visual Studio"/2019/Enterprise/MSBuild/Current/Bin/msbuild.exe "$APPCENTER_SOURCE_DIRECTORY\AmethystWindows\AmethystWindows.csproj" /p:Configuration=$APPCENTER_UWP_CONFIGURATION /p:Platform=ARM
 
-C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/MSBuild/Current/Bin/msbuild.exe $APPCENTER_SOURCE_DIRECTORY\AmethystWindows\AmethystWindows.csproj /p:Configuration=$APPCENTER_UWP_CONFIGURATION /p:Platform=x86
+C:/"Program Files (x86)"/"Microsoft Visual Studio"/2019/Enterprise/MSBuild/Current/Bin/msbuild.exe "$APPCENTER_SOURCE_DIRECTORY\AmethystWindows\AmethystWindows.csproj" /p:Configuration=$APPCENTER_UWP_CONFIGURATION /p:Platform=x86
 
-C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/MSBuild/Current/Bin/msbuild.exe $APPCENTER_SOURCE_DIRECTORY\AmethystWindows\AmethystWindows.csproj /p:Configuration=$APPCENTER_UWP_CONFIGURATION /p:Platform=x64
+C:/"Program Files (x86)"/"Microsoft Visual Studio"/2019/Enterprise/MSBuild/Current/Bin/msbuild.exe "$APPCENTER_SOURCE_DIRECTORY\AmethystWindows\AmethystWindows.csproj" /p:Configuration=$APPCENTER_UWP_CONFIGURATION /p:Platform=x64
